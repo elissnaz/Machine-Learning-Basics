ac6dc053793d2fb815dd5f91707385219c6a89f6
Elis Naz ARI <aelisnaz@gmail.com>
2026-02-23 14:06:26 +0300

    Add regression models for prediction module

---
+++ 2.Prediction/2_multiple_linear_regression.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 18 14:30:29 2026
+
+@author: USER
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#COKLU DEĞİSKEN ICIN VERİ HAZIRLAMA
+
+veriler = pd.read_csv('data.csv')
+print(veriler)
+
+ulke = veriler.iloc[:,0:1].values
+Yas =veriler.iloc[:,1:4].values
+
+from sklearn import preprocessing
+le= preprocessing.LabelEncoder()
+ulke[:,0]=le.fit_transform(veriler.iloc[:,0])
+print(ulke)
+
+ohe=preprocessing.OneHotEncoder()
+ulke= ohe.fit_transform(ulke).toarray()
+print(ulke)
+
+c = veriler.iloc[:,-1:].values
+
+le= preprocessing.LabelEncoder()
+c[:,-1]=le.fit_transform(veriler.iloc[:,-1])
+print(c)
+
+ohe=preprocessing.OneHotEncoder()
+c= ohe.fit_transform(c).toarray()
+print(c)
+
+
+
+sonuc= pd.DataFrame(data=ulke, index=range(22),columns=['fr','tr','us'])
+print(sonuc)   
+
+sonuc2= pd.DataFrame(data=Yas, index=range(22),columns=['boy','kilo','yas'])
+print(sonuc2) 
+
+sonuc3= pd.DataFrame(data=c[:,:1], index=range(22),columns=['cinsiyet'])
+print(sonuc3)
+
+s=pd.concat([sonuc,sonuc2],axis=1)#0 olsa alt alta eklerdi
+print(s)
+
+s2=pd.concat([s,sonuc3],axis=1)
+print(s2)
+
+
+from sklearn.model_selection import train_test_split
+
+x_train, x_test,y_train,y_test = train_test_split(s,sonuc3,test_size=0.33, random_state=0)
+
+#verilerin olceklenmesi
+from sklearn.preprocessing import StandardScaler
+
+sc=StandardScaler()
+
+X_train = sc.fit_transform(x_train)
+X_test = sc.fit_transform(x_test)
+
+#COKLU LINEER REGRESYON
+from sklearn.linear_model import LinearRegression
+regressor= LinearRegression()
+regressor.fit(x_train, y_train)
+
+y_pred= regressor.predict(x_test)
+
+#veri manipüle s2 de boy sutununu ayırdım
+boy= s2.iloc[:,3:4].values
+sol= s2.iloc[:,:3]
+sag= s2.iloc[:,4:]
+
+veri= pd.concat([sol,sag],axis=1)
+
+x_train, x_test,y_train,y_test = train_test_split(veri,boy,test_size=0.33, random_state=0)
+
+r2= LinearRegression()
+r2.fit(x_train, y_train)
+y_pred=r2.predict(x_test)
+
